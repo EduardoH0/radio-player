2b3a17218f1abefd0698de3c5771861c5f54e653
EduardoHerrerosMC <eherreros@managingcomposites.com>
2026-09-19 15:38:56 +0200

    Initial commit

---
+++ src/models/radio-station.ts
@@ -0,0 +1,11 @@
+// radio-station.ts
+
+
+export interface RadioStation {
+    id: string;
+    title: string;
+    url: string;
+    country?: string;
+    quality?: string;
+    image?: string;
+}
